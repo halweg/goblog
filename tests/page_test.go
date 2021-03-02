d355c8686023eced2419719e750cefa898a4bf92
halweg <halweg@126.com>
2021-03-02 16:29:10 +0800

    添加测试代码

--- tests/page_test.go
+++ tests/page_test.go
@@ -3,41 +3,47 @@ package tests
 import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"strconv"
 	"testing"
 )
 
-func TestHomePage(t *testing.T)  {
-	baseURL := "http://localhost:3000"
-
-	var (
-		resp *http.Response
-		errors error
-	)
-
-	resp, errors = http.Get(baseURL+ "/")
-
-	assert.NoError(t, errors, "有错误发生，errors不为空")
-	assert.Equal(t, 200, resp.StatusCode, "应该返回状态码200")
-}
-
-func TestAboutPage(t *testing.T) {
-	baseURL := "http://localhost:3000"
-
-	var (
-		resp *http.Response
-		err error
-	)
-
-	resp, err = http.Get(baseURL + "/about")
-
-	assert.NoError(t, err, "有错误发生，err不为空！")
-	assert.Equal(t, 200, resp.StatusCode, "应该返回状态码200")
-}
 
 func TestAllPage(t *testing.T)  {
+	baseURL := "http://localhost:3000"
 
+	tests := []struct {
+		url      string
+		method   string
+		expected int
+	}{
+	 	{"/", "GET", 200},
+	 	{"/about", "GET", 200},
+	 	{"/notfound", "GET", 404},
+	 	{"/articles/1", "GET", 200},
+	 	{"/articles", "GET", 200},
+	 	{"/articles/create", "GET", 200},
+	 	{"/articles", "POST", 200},
+	 	{"/articles/2/edit", "GET", 200},
+	 	{"/articles/2", "GET", 200},
+	 	{"/articles/-1", "POST", 404},
+	}
 
+	for _, test := range tests {
+		var (
+			resp *http.Response
+			err error
+		)
 
+		switch {
+		case test.method == "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseURL+test.url, data)
+		default:
+			resp, err = http.Get(baseURL+test.url)
+		}
 
+		assert.NoError(t, err, "请求"+test.url+"有错误发生，err不为空！")
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+"应该返回状态码"+strconv.Itoa(test.expected))
+	}
 
 }
