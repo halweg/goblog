2e542eee7dcc5a7d8a73f96a9dfed0b3c89191f2
halweg <halweg@126.com>
2021-03-04 16:30:42 +0800

    删除无用变量定义

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
 	"net/http"
 )
 
-var router *mux.Router
-
 func main() {
 
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
 	// 中间件：强制内容类型为 HTML
 	router.Use(middlewares.ForceHTMLMiddleware)
