20752c01fe63bf83270d8f4d5f446a59da2c3fc7
halweg <halweg@126.com>
2021-03-05 22:44:04 +0800

    重构文章表单验证

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -1,14 +1,14 @@
 package controllers
 
 import (
-	"fmt"
-	"goblog/app/models/article"
-	"goblog/pkg/logger"
-	"goblog/pkg/route"
-	"goblog/pkg/view"
-	"gorm.io/gorm"
-	"net/http"
-	"unicode/utf8"
+    "fmt"
+    "goblog/app/models/article"
+    "goblog/app/requests"
+    "goblog/pkg/logger"
+    "goblog/pkg/route"
+    "goblog/pkg/view"
+    "gorm.io/gorm"
+    "net/http"
 )
 
 type ArticlesController struct {
@@ -18,25 +18,6 @@ type ArticlesFormData struct {
 	Title, Body string
 	Article     article.Article
 	Errors      map[string]string
-}
-
-func validateArticleFormData(title string, body string) map[string]string {
-	errors := make(map[string]string)
-	// 验证标题
-	if title == "" {
-		errors["title"] = "标题不能为空"
-	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
-		errors["title"] = "标题长度需介于 3-40"
-	}
-
-	// 验证内容
-	if body == "" {
-		errors["body"] = "内容不能为空"
-	} else if utf8.RuneCountInString(body) < 10 {
-		errors["body"] = "内容长度需大于或等于 10 个字节"
-	}
-
-	return errors
 }
 
 func (ac *ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
@@ -79,33 +60,31 @@ func (ac *ArticlesController) Create(w http.ResponseWriter, r *http.Request) {
 }
 
 func (ac *ArticlesController) Store(w http.ResponseWriter, r *http.Request) {
-	title :=  r.FormValue("title")
-	body := r.FormValue("body")
 
-	errors := validateArticleFormData(title, body)
+	// 1. 初始化数据
+	_article := article.Article{
+		Title: r.PostFormValue("title"),
+		Body:  r.PostFormValue("body"),
+	}
 
+	// 2. 表单验证
+	errors := requests.ValidateArticleForm(_article)
+
+	// 3. 检测错误
 	if len(errors) == 0 {
-		_article := article.Article{
-			Title: title,
-			Body:  body,
-		}
+		// 创建文章
 		_article.Create()
 		if _article.ID > 0 {
-			fmt.Fprint(w, "插入成功，ID 为"+_article.GetStringID())
+			indexURL := route.Name2URL("articles.show", "id", _article.GetStringID())
+			http.Redirect(w, r, indexURL, http.StatusFound)
 		} else {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprint(w, "创建文章失败，请联系管理员")
 		}
-
 	} else {
 		view.Render(w, view.D{
-			"Title":  title,
-			"Body":   body,
-			"Errors": errors,
-			"Article" : article.Article{
-				Title:     title,
-				Body:      body,
-			},
+			"Article": _article,
+			"Errors":  errors,
 		}, "articles.create", "articles._form_field")
 	}
 }
@@ -132,8 +111,6 @@ func (ac *ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// 4. 读取成功，显示编辑文章表单
 		view.Render(w, view.D{
-			"Title":   _article.Title,
-			"Body":    _article.Body,
 			"Article": _article,
 		}, "articles.edit", "articles._form_field")
 	}
@@ -156,10 +133,11 @@ func (ac *ArticlesController) Update(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "服务器内部错误！")
 		}
 	} else {
+
 		_article.Title = r.PostFormValue("title")
 		_article.Body = r.PostFormValue("body")
 
-		errors := validateArticleFormData(_article.Title, _article.Body)
+		errors := requests.ValidateArticleForm(_article)
 
 		if len(errors) == 0 {
 			rowNum, err := _article.Update()
@@ -179,8 +157,6 @@ func (ac *ArticlesController) Update(w http.ResponseWriter, r *http.Request) {
 		} else {
 			// 4.3 表单验证不通过，显示理由
 			view.Render(w, view.D{
-				"Title":   _article.Title,
-				"Body":    _article.Body,
 				"Article": _article,
 				"Errors":  errors,
 			}, "articles.edit", "articles._form_field")
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module goblog
 go 1.15
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/sessions v1.2.1
 	github.com/spf13/cast v1.3.0
